9f5624e0be0daed8252d96f6951c1480f7a566c5
null-d3v <benajmin.soddy@gmail.com>
2021-12-27 04:29:14 -0600

    fix: config set

--- tests/System/MessagingTest.cs
+++ tests/System/MessagingTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Xunit;
 
 namespace L1L2RedisCache.Test.System;
@@ -13,9 +14,16 @@ public class MessagingTest
             .AddJsonFile("appsettings.json")
             .AddEnvironmentVariables()
             .Build();
+        NotifyKeyspaceEventsConfig = new Dictionary<MessagingType, string>
+        {
+            { MessagingType.Default, string.Empty },
+            { MessagingType.KeyeventNotifications, "ghE" },
+            { MessagingType.KeyspaceNotifications, "ghK" },
+        };
     }
 
     public IConfiguration Configuration { get; }
+    public IDictionary<MessagingType, string> NotifyKeyspaceEventsConfig { get; }
 
     [InlineData(MessagingType.Default)]
     [InlineData(MessagingType.KeyeventNotifications)]
@@ -34,6 +42,22 @@ public class MessagingTest
         var primaryL1L2Cache = primaryServices
             .BuildServiceProvider()
             .GetRequiredService<IDistributedCache>();
+        var primaryL1L2CacheOptions = primaryServices
+            .BuildServiceProvider()
+            .GetRequiredService<IOptions<L1L2RedisCacheOptions>>()
+            .Value;
+
+        var primaryDatabase = (await primaryL1L2CacheOptions
+            .ConnectionMultiplexerFactory())
+            .GetDatabase(
+                primaryL1L2CacheOptions
+                    .ConfigurationOptions?
+                    .DefaultDatabase ?? -1);
+        await primaryDatabase.ExecuteAsync(
+            "config",
+            "set",
+            "notify-keyspace-events",
+            NotifyKeyspaceEventsConfig[messagingType]);
 
         var secondaryServices = new ServiceCollection();
         secondaryServices.AddSingleton(Configuration);
